a48cc755b932796b1779beac0ee01179fc5ef2a4
0x07CB <83157348+0x07CB@users.noreply.github.com>
2023-06-13 11:44:44 +0000

    worker new theme added (mods from ys theme)

--- usr/theme/worker/worker.theme.sh
+++ usr/theme/worker/worker.theme.sh
@@ -1,14 +1,13 @@
 # OhMySh Theme Worker
 
-# function for check if user is root
-function _ROOT(){
-  if [ "$UID" -eq 0 ]
-  then
-    IS_ROOT_ = "${COLOR[red]}(root)${COLOR[reset]}"
-  else
-    IS_ROOT_ = "${COLOR[green]}(user)${COLOR[reset]}"
-  fi
-}
+# check if user is root
+IS_ROOT_=""
+if [ "$UID" -eq 0 ]
+then
+  IS_ROOT_="[ROOT]"
+else
+  IS_ROOT_="$(echo $HOME)"
+fi
 
 function _GIT_T(){
   if [ -n "$(OMS_GIT)" ]
@@ -17,5 +16,16 @@ function _GIT_T(){
   fi
 }
 
-PS1="\n$IS_ROOT_\n${COLOR[blue]}#${COLOR[reset]} ${COLOR[cyan]}\u${COLOR[reset]} @ ${COLOR[green]}\h${COLOR[reset]} in ${COLOR[brown]}\w${COLOR[reset]} \$(_GIT_T) [\$(date +%H:%M:%S)]\n${COLOR[red]}\$${COLOR[reset]} "
+# function to call _GIT_T and if error return empty and echo error to /dev/null
+function OMS_GIT(){
+  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
+}
+
+#PS1="\n$IS_ROOT_\n${COLOR[blue]}#${COLOR[reset]} ${COLOR[cyan]}\u${COLOR[reset]} @ ${COLOR[green]}\h${COLOR[reset]} in ${COLOR[brown]}\w${COLOR[reset]} \$(_GIT_T) [\$(date +%H:%M:%S)]\n${COLOR[red]}\$${COLOR[reset]} "
+# new PS1 but # is cyan and the name of user is green the '@' is brown and the name of host is white 'in' is brown and the path is green (all that at left) and the time is cyan and the hour is green at right. the $ is red if user is not root , else is red #.
+if [[ "$(id -u)" != "0" ]]; then
+  PS1="[${COLOR[cyan]}\$(date +%H:%M:%S)${COLOR[reset]}] - $IS_ROOT_ \n${COLOR[cyan]}#${COLOR[reset]} ${COLOR[green]}\u${COLOR[reset]} ${COLOR[brown]}@${COLOR[reset]} ${COLOR[white]}\h${COLOR[reset]} ${COLOR[brown]}in${COLOR[reset]} ${COLOR[green]}\w${COLOR[reset]} \$(_GIT_T) \n ${COLOR[red]}\$${COLOR[reset]} "
+elif [[ "$(id -u)" == "0" ]]; then
+  PS1="[${COLOR[cyan]}\$(date +%H:%M:%S)${COLOR[reset]}] - $IS_ROOT_ \n${COLOR[cyan]}#${COLOR[reset]} ${COLOR[green]}\u${COLOR[reset]} ${COLOR[brown]}@${COLOR[reset]} ${COLOR[white]}\h${COLOR[reset]} ${COLOR[brown]}in${COLOR[reset]} ${COLOR[green]}\w${COLOR[reset]} \$(_GIT_T) \n ${COLOR[red]}#${COLOR[reset]} "
+fi
 
